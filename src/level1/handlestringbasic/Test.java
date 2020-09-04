ae180a01412ded3f7d70dcbad50175a725d198b9
Lokie89 <traeuman@gmail.com>
2020-09-04 17:13:36 +0900

    handlestringbasic

---
+++ src/level1/handlestringbasic/Test.java
@@ -0,0 +1,12 @@
+package level1.handlestringbasic;
+
+public class Test {
+    public static void main(String[] args) {
+        String s = "a234";
+        String s2 = "1234";
+        Solution solution = new Solution();
+
+        System.out.println(solution.solution(s));
+        System.out.println(solution.solution(s2));
+    }
+}
