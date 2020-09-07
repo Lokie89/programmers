fc9d93323641c65bc73d2103b4acaa915f515382
Lokie89 <traeuman@gmail.com>
2020-09-07 15:22:56 +0900

    findprimenumber

---
+++ src/level1/findprimenumber/Test.java
@@ -0,0 +1,15 @@
+package level1.findprimenumber;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(2));
+        System.out.println(new Solution().solution(3));
+        System.out.println(new Solution().solution(4));
+        System.out.println(new Solution().solution(5));
+        System.out.println(new Solution().solution(6));
+        System.out.println(new Solution().solution(7));
+        System.out.println(new Solution().solution(8));
+        System.out.println(new Solution().solution(9));
+        System.out.println(new Solution().solution(10));
+    }
+}
