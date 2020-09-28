e98b90ba7d89b67936e74a75db0cb8e162ad6e2d
Lokie89 <traeuman@gmail.com>
2020-09-29 08:44:05 +0900

    hidephonenum

---
+++ src/level1/hidephonenum/Solution.java
@@ -0,0 +1,22 @@
+package level1.hidephonenum;
+
+public class Solution {
+    public String solution(String phone_number) {
+        validateLength(phone_number);
+        int length = phone_number.length();
+
+        String answer = "";
+        for (int i = 0; i < length - 4; i++) {
+            answer += "*";
+        }
+        answer += phone_number.substring(length - 4, length);
+        return answer;
+    }
+
+    // s는 길이 4 이상, 20이하인 문자열입니다
+    private void validateLength(String s) {
+        if (s.length() < 4 || s.length() > 20) {
+            throw new RuntimeException();
+        }
+    }
+}
---
+++ src/level1/hidephonenum/Test.java
@@ -0,0 +1,8 @@
+package level1.hidephonenum;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("01033334444"));
+        System.out.println(new Solution().solution("027778888"));
+    }
+}
