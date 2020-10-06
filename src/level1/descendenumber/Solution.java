93966e1b2394041c58966dbc7b3d894277870e7c
Lokie89 <traeuman@gmail.com>
2020-10-06 15:45:27 +0900

    descendenumber

--- src/level1/descendenumber/Solution.java
+++ src/level1/descendenumber/Solution.java
@@ -1,30 +1,25 @@
 package level1.descendenumber;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 // https://programmers.co.kr/learn/courses/30/lessons/12933?language=java
-// TODO
 public class Solution {
     public long solution(long n) {
-        validateNumber(n);
-        Integer[] arr = new Integer[String.valueOf(n).length()];
-        int index = 0;
-        while (n > 0) {
-            arr[index++] = ((int) n % 10);
-            n /= 10;
+        validateN(n);
+        String strN = String.valueOf(n);
+        Character[] characters = strN.chars().mapToObj(value -> (char)value).toArray(Character[]::new);
+        Arrays.sort(characters, Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for(char c: characters){
+            sb.append(c);
         }
-        Arrays.sort(arr);
-        long answer = 0;
-        int pow = 0;
-        for (int a : arr) {
-            answer += a * Math.pow(10, pow++);
-        }
-
+        long answer = Long.parseLong(sb.toString());
         return answer;
     }
 
-    // n 은 1이상 8000000000 이하인 자연수입니다.
-    private void validateNumber(long n) {
+    // n은 1이상 8000000000 이하인 자연수입니다.
+    private void validateN(long n) {
         if (n < 1 || n > 8000000000L) {
             throw new RuntimeException();
         }
