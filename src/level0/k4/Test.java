cce5a172bb761165f0ae46dec7d4bb08f33f3e64
Lokie89 <traeuman@gmail.com>
2020-10-10 13:45:36 +0900

    gitignore

--- src/level0/k4/Test.java
+++
@@ -1,9 +0,0 @@
-package level0.k4;
-
-import level0.k1.Solution;
-
-public class Test {
-    public static void main(String[] args) {
-        new Solution();
-    }
-}
--- src/level0/k6/Test.java
+++
@@ -1,9 +0,0 @@
-package level0.k6;
-
-import level0.k1.Solution;
-
-public class Test {
-    public static void main(String[] args) {
-        new Solution();
-    }
-}
