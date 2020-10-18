afbf0129362a2f28553f89d9523a086b5cef6f88
Lokie89 <traeuman@gmail.com>
2020-10-18 18:01:39 +0900

    compresstring

---
+++ src/level2/compressstring/Test.java
@@ -0,0 +1,11 @@
+package level2.compressstring;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("aabbacc"));
+        System.out.println(new Solution().solution("ababcdcdababcdcd"));
+        System.out.println(new Solution().solution("abcabcdede"));
+        System.out.println(new Solution().solution("abcabcabcabcdededededede"));
+        System.out.println(new Solution().solution("xababcdcdababcdcd"));
+    }
+}
